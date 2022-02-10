34e283b84db14a1909300fd4f4603bd0a1bf2b57
dhaval <dhagrawal0@gmail.com>
2022-02-10 16:39:00 +0530

    added stock buy and sell problem

--- src/Day1/StockBuySell.java
+++ src/Day1/StockBuySell.java
@@ -1,5 +1,25 @@
-package Day1;
+class Solution {
+    public int maxProfit(int[] prices) {
 
-public class StockBuySell {
+        //brute force: using DP
 
+        //another approach: using nested for loops
+
+        //optimal approach: traverse the array from left to right and store the min value encountered so far
+        //calculate the max profit by finding the difference between curr val and min val
+
+        int minTillNow = prices[0];
+        int maxProfit =0;
+        int size = prices.length;
+
+
+        for(int i=1;i<size;i++){
+            maxProfit = Math.max(maxProfit,prices[i]-minTillNow);
+
+            minTillNow = Math.min(minTillNow,prices[i]);
+        }
+
+        return maxProfit;
+
+    }
 }
